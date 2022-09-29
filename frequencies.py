123c2bb4fccb84d93aec9cce50582f7844ffd44c
Ronen09 <ronen.roy@kcl.ac.uk>
2022-09-29 09:24:40 +0100

    modified frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,13 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
+    items = list(map(str, items));
     frequencies = {}
+    for item in items:
+        if item in frequencies:
+            frequencies[item] += 1;
+        else:
+            frequencies[item] = 1;
     # Your code goes here
     return frequencies
+
